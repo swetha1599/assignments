84c62733b87e78faf82f5f4fca1e2914bc897806
swetha <tswetha950@gmail.com>
2022-05-14 10:27:30 +0530

    new files added

---
+++ registration/index.js
@@ -0,0 +1,13 @@
+
+
+
+function validate(){
+    let firstName = document.getElementById('firstName').value;
+let lastName = document.getElementById('lastName').value;
+let phone = document.getElementById('phone').value;
+let email = document.getElementById('email').value;
+let state = document.getElementById('state').value;
+let zipcode = document.getElementById('zip').value;
+    console.log('validation works fine!!');
+    console.log(firstName,lastName,phone,state,email,zip);
+}
